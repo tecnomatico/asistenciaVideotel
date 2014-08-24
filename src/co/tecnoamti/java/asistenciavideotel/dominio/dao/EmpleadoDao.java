b0b1677deabc54f64c9b5e2d0f7ff918c0b15657
joel <joel@joel-dell>
2014-08-24 18:35:47 -0300

    primera subida- bd

---
+++ src/co/tecnoamti/java/asistenciavideotel/dominio/dao/EmpleadoDao.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.tecnoamti.java.asistenciavideotel.dominio.dao;
+
+/**
+ *
+ * @author joel
+ */
+public interface EmpleadoDao {
+    
+}
---
+++ src/co/tecnoamti/java/asistenciavideotel/dominio/dao/imp/EmpleadoDaoImp.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.tecnoamti.java.asistenciavideotel.dominio.dao.imp;
+
+/**
+ *
+ * @author joel
+ */
+public class EmpleadoDaoImp {
+    
+}
